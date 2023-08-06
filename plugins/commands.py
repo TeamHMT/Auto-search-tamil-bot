f7d7fc05106f73a075d914b02d0b816f4107651c
divytech <95929956+divytech@users.noreply.github.com>
2023-08-06 12:28:31 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -54,7 +54,7 @@ async def start(client, message):
                     InlineKeyboardButton('✇ Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ✇', url=CHNL_LNK)
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        m=await message.reply_sticker("CAACAgUAAxkBAAIBYmOkbpQ0brb4z4ggbPv4peTgcDb-AAIlBQACSr5ZVk64AdT0N9vYHgQ") 
+        m=await message.reply_sticker("CAACAgUAAxkBAAEJ7yFkz0OreufWw2wMDeKhdt-zuYOd9AACcwoAAuUggVaUAAHxd1i5oU4vBA") 
         await asyncio.sleep(1)
         await m.delete()
         await message.reply_photo(
