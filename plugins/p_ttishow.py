38f57cf71227ade758fd4e4b00eb5e9b324ef0ce
divytech <95929956+divytech@users.noreply.github.com>
2023-08-20 19:04:02 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -45,7 +45,7 @@ async def save_group(bot, message):
                   ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
-            text=f"<b>Thankyou For Adding Me In {message.chat.title} ❣️\n\nIf you have any questions & doubts about using me contact support.</b>",
+            text=f"<b>Thankyou For Adding Me In {message.chat.title} ❣️\n\nIf you have any questions & doubts about using me contact <a href="https://t.me/dev8953">Oᴡɴᴇʀ 😎</a>.</b>",
             reply_markup=reply_markup)
     else:
         settings = await get_settings(message.chat.id)
